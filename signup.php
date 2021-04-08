504648a1160f24a13fcf30367dd13e389326f1d1
Krithikha Bala <59526292+krithikha2001@users.noreply.github.com>
2021-04-08 18:08:37 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -16,7 +16,6 @@
     if (empty($_POST["name"])) {
       $nameErr = "Name is required" ;
     } else {
-   //   $name = test_input($_POST["name"]);
       // check if name only contains letters and whitespace
       if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
         $nameErr = "Only letters and white space allowed";
