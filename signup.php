87211fe9d8e02375dcc1cb3b937c58d5c7274e4e
Krithikha Bala <59526292+krithikha2001@users.noreply.github.com>
2021-04-09 16:06:43 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -30,10 +30,14 @@
     
     if(($password == $cpassword) && $exists==false && $f==0){
         $sql = "INSERT INTO `login` ( `login_username`, `password`) VALUES ('$username', '$password')";
+        $sql = "INSERT INTO `customer` (`Cust_name`,`gender`,`email`,`phone_number`,`login_username`) VALUES ('$name','$gender','$email','$phno','$username')";
         $result = mysqli_query($conn, $sql);
         if ($result){
             $showalert = true;
-        } 
+        }
+        else
+        echo("Error description: " . mysqli_error($conn));
+ 
     }
     else if($password != $cpassword){
         $showError = "Error inserting.!Check if password and confirm password match";   
