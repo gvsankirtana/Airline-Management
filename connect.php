e94d56f8e529acbd27688a791a2722e2fd67f884
Krithikha Bala <59526292+krithikha2001@users.noreply.github.com>
2021-04-10 11:12:02 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,16 @@
 <?php
-$conn=mysqli_connect("localhost", "root", "", "flight_management") or die(mysqli_error($conn));
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "flight_management";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+if ($conn){
+    echo "success";
+ }
+ else{
+     echo "fail:";
+    die("Error". mysqli_connect_error());
+}
+
 ?>
