783be1ba179fbf205830ec903bf2564145be080a
mahitha2001 <mahithamilli@gmail.com>
2021-04-24 12:20:41 +0530

    trying

--- ticket.php
+++ ticket.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
 require 'connect.php';
+$BankName=mysqli_real_escape_string($conn,$_POST["BankName"]);
+$AccountNumber=mysqli_real_escape_string($conn,$_POST["AccountNumber"]);
 $flightid=$_SESSION['flightid'];
 $class=$_SESSION['class'];
 $aadhar=$_SESSION['adhaar'];
+$number=$_POST['passengers'];
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $BankName=mysqli_real_escape_string($conn,$_POST["BankName"]);
-    $AccountNumber=mysqli_real_escape_string($conn,$_POST["AccountNumber"]);
-    $query = "INSERT INTO ticket (class, payment_Type, booking_time, booking_date, account_No, Bank_name, flight_ID, aadhar_no) values('$class', 'UPI', CURRENT_TIME(), CURRENT_DATE(), '$AccountNumber', '$BankName', '$flightid', '$aadhar')";
-    $result = mysqli_query($conn, $query); 
+
+    $query = "INSERT INTO ticket (Booking_Ref, class, payment_Type, booking_time, booking_date, account_No, Bank_name, flight_ID, aadhar_no) values('$class', 'UPI', CURRENT_TIME(), CURRENT_DATE(), '$AccountNumber', '$BankName', '$flightid', '$aadhar')";
+    $result = mysqli_query($conn, $query);
+    $query = "SELECT Ticket_no FROM ticket WHERE aadhar_no='$aadhar'";
+    $result =mysqli_query($conn,$query);
+    echo $result."#".$flightid;
     $query = "UPDATE airline SET vacant_seats=(SELECT vacant_seats FROM airline WHERE Flight_ID = '$flightid')-1 WHERE Flight_ID = '$flightid'";
     $result = mysqli_query($conn, $query);
-    echo $query;
-    echo $result;
     header ("location: searchflights.php");
 }
 ?>
