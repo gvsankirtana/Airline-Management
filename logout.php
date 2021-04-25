99c58c61af940ca699bc6dd3d75d68294f9af40b
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2021-04-25 09:40:42 +0300

    updated logout

--- logout.php
+++ logout.php
@@ -1,6 +1,4 @@
 <?php
-header('Cache-Control: no cache'); //no cache
-session_cache_limiter('private_no_expire'); // works
 session_start();
 if(session_destroy()){
     header("location:homepage.php");
