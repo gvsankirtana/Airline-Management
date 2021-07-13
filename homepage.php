237b0a7840943d559fa04b9717ef52c5f0f71696
mahitha2001 <mahithamilli@gmail.com>
2021-07-13 23:03:46 +0530

    Updated homepage.php

--- homepage.php
+++ homepage.php
@@ -5,7 +5,7 @@ session_start();
 
 <!doctype html>
   
-<html lang="en">
+<html lang="en">   
   <head>
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
